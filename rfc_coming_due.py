dfcb29af5506d23db4e4a2d0d9d5c93849aebd45
dscheiber <dscheiber@live.com>
2021-09-30 14:41:55 -0400

    Create rfc_coming_due.py
    A python script for automating status update emails

---
+++ rfc_coming_due.py
@@ -0,0 +1,56 @@
+import win32com.client as win32
+import csv
+
+test_csv_raw = '''"C 21 097",6/2/2021 0:00:00,"Revise all active SE Stability Protocols ","reference current Product Specifications","Emerson","Open","MLR",6/30/2021 0:00:00,9/30/2021 0:00:00,,9/30/2021 0:00:00,"LAF","First","daniel scheiber <daniel.scheiber@pacelabs.com>")'''
+test_csv_split = test_csv_raw.split(',')
+
+#this is a row of data used for testing the sendMail function
+testLine = ['C 21 151', '08/24/2021 0:00:00', 'Revise SP/4090/001, SP/5049/001-0004.00 to .01', 'correct # of water determinations required', 'XW Pharma', 'Open', 'MLR', '09/30/2021 0:00:00', '', '', '09/30/2021 0:00:00', 'LAF', 'N/A', 'Samantha McLaughlin <Samantha.McLaughlin@pacelabs.com>']
+
+##query file
+queryFile = "C:\\users\\dscheiber\\documents\\Due End of Month CCs - Emails - dscheiberonlytest.txt"
+
+def sendMail(queryline):
+    ##stages all the csv line variables
+    rfc_number = queryline[0]
+    rfc_initiation = queryline[1][:10]
+    rfc_task = queryline[2]
+    rfc_reason = queryline[3]
+    rfc_due_date = queryline[10][:10]
+    rfc_current_extension = queryline[12]
+    rfc_email = queryline[13]
+
+    ##stages all the email variables
+    subject = 'Change Control Reminder for ' + rfc_number
+    body = 'Hi, \nThis is an automated reminder that the following RFC is coming due: \n' + \
+    'RFC#: ' + rfc_number + '\n' + \
+    'Task: ' + rfc_task + '\n' + \
+    'Description: ' + rfc_reason + '\n' + \
+    'Due Date: ' + rfc_due_date + '\n' + \
+    'Current Extension: ' + rfc_current_extension + '\n' + '\n' \
+    'If there are any questions or corrections, please reply to this email. Thanks.'
+    #htmlbody = '<h2> Using HTML Message body </h2>'
+
+    #send the email
+    outlook = win32.Dispatch('outlook.application')
+    mail = outlook.CreateItem(0)
+    mail.To = rfc_email
+    mail.Subject = subject
+    mail.Body = body
+    #mail.HTMLBody = htmlbody #this field is optional
+
+    # To attach a file to the email (optional):
+    #attachment  = "Path to the attachment"
+    #mail.Attachments.Add(attachment)
+
+    mail.Send()
+
+def readQuery(queryFile):
+    with open(queryFile, newline='') as csvfile:
+	    reader = csv.reader(csvfile, delimiter="¿", quotechar='"')
+	    for row in reader:
+		    sendMail(row)
+
+
+readQuery(queryFile)
+#sendMail(testLine)
