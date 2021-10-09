e9fc54217df672b160945ce348d0695d67a9bea6
dscheiber <dscheiber@live.com>
2021-10-09 14:39:24 -0400

    got a user filter working, but need to develop this for metrics calculation and visualization

--- rfc_using_pandas.py
+++ rfc_using_pandas.py
@@ -1,20 +1,29 @@
 import pandas
 import time
 
-excelDataDF = pandas.read_excel('W:\\Change Control\\CHANGE CONTROL LOG.xlsm', sheet_name="MasterList")
+excelDataDF = pandas.read_excel('Test-RFC.xlsm', sheet_name="MasterList")
 
-Q3Start = "2021-07-01"
-#Q3StartConverted = time.strptime(Q3Start, '%Y-%m-%d')
-#print(Q3StartConverted)
-#print("Passed the start.")
-Q3End = "2021-09-30"
-#Q3EndConverted = time.strptime(Q3End, '%Y-%m-%d')
-#print(Q3EndConverted)
-#print('Passed the end.')
-
-open_date_filter = excelDataDF.query('`INITIATION DATE`>= @Q3Start and `INITIATION DATE`<= @Q3End')
-opened_count = open_date_filter.shape(0)
+periodStart = "2021-07-01"
+periodEnd = "2021-09-30"
 
 
+## filters the main db by date and sorts by requester
+open_date_filter = excelDataDF.query('`INITIATION DATE`>= @periodStart and `INITIATION DATE`<= @periodEnd').sort_values(by='REQUESTER', ascending=True)
+closed_date_filter = excelDataDF.query('`DATE CLOSED`>= @periodStart and `DATE CLOSED`<= @periodEnd').sort_values(by='REQUESTER', ascending=True)
 
-print(opened_count)
+def filter_by_users(df): ## gets a user list which will be used to loop through the values
+    users_from_df = df.REQUESTER
+    users = []
+    for user in users_from_df:
+        if user not in users:
+            users.append(user)
+
+    for user in users:
+        user_filter = df.query('`REQUESTER` == @user')
+        print(user_filter)
+
+    ##to finish this section, i need to either filter or format these in various ways, maybe apply a function that says DUE SOON or PAST DUE to late ones
+
+def rfc_metrics(df_open, df_closed):
+    initiated_rfc = df_open.shape[0]
+    closed_rfc = df_closed.shape[0]
